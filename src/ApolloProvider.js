1b2ac27c3953f55839487d8d4245c096346ddf75
Mutukundu Mahendra Reddy <reddymahendra52@gmail.com>
2021-09-28 13:54:34 +0530

    updated links

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000'
+  uri: 'https://polar-cliffs-80764.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
