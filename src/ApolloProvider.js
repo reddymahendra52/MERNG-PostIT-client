be7daf132bec5096f3440191cf70cb43a27af146
reddymahendra52 <61696212+reddymahendra52@users.noreply.github.com>
2022-03-12 19:41:17 +0530

    Updated Server Link

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'https://polar-cliffs-80764.herokuapp.com/'
+  uri: 'https://reddymahendra52-walcan-server.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
